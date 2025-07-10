e30e28dad9d7b0684bafcfc5cf032192aa8c5241
Saravanan Ponnaiah <saravanan.289@gmail.com>
2025-07-10 21:17:52 +0530

    Commit: Added deployment steps

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,4 +1,4 @@
-from src.main import add, subtract
+from src.main import add, subtract, get_date
 
 def test_add():
     assert add(10, 20) == 30
@@ -8,4 +8,8 @@ def test_add():
 def test_subtract():
     assert subtract(30, 10) == 20
     assert subtract(100, 25) == 75
+    
+def test_get_date():
+    assert str(get_date('2025-07-10 21:08:00')) == '2025-07-10'
+    assert str(get_date('2024-12-12 10:10:10')) == '2024-12-12'
     
